18a33aedc1b6a769bb95f20f43772b6c642cc430
Chris McCord <chris@chrismccord.com>
2026-08-27 13:40:31 -0400

    Release 0.1.5

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule DurableServer.MixProject do
   def project do
     [
       app: :durable_server,
-      version: "0.1.4",
+      version: "0.1.5",
       elixir: "~> 1.19",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
